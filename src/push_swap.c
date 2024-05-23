9695dedf561a6ccabc78939b88dc3fb525e51d88
Toky <hello@toky.tech>
2024-05-23 08:57:49 +0200

    add input parsing

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,28 +6,47 @@
 /*   By: franaivo <tokyfy@outlook.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 18:20:56 by franaivo          #+#    #+#             */
-/*   Updated: 2024/05/20 19:22:59 by franaivo         ###   ########.fr       */
+/*   Updated: 2024/05/22 21:51:59 by franaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   src.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: franaivo <franaivo@student.42antananariv>  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/11 11:05:29 by franaivo          #+#    #+#             */
-/*   Updated: 2024/05/20 18:29:49 by franaivo         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 #include "push_swap.h"
+
+int ft_is_all_digit(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        if (!ft_isdigit(str[i]))
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+char ** parse_input(int argc , char *argv[])
+{
+    char *str = ft_strdup("");
+    for (int i = argc - 1; i > 0; i--)
+    {
+        if(!ft_is_all_digit(argv[i]))
+        {
+            return (NULL);
+        }
+        char *temp = ft_strjoin(str, " "); // Add a space before the number
+        char *temp2 = ft_strjoin(temp, argv[i]);
+        free(temp);
+        free(str);
+        str = temp2;
+    }
+    return (ft_split(str, ' '));
+}
 
 int	main(int argc, char *argv[])
 {
 	t_stack	*stack_b;
 	t_stack	*stack_a;
-	int		num;
+	char    **input;ls
 
 	if (argc < 2)
 	{
@@ -36,11 +55,16 @@ int	main(int argc, char *argv[])
 	}
 	stack_b = ft_stack_new();
 	stack_a = ft_stack_new();
-	for (int i = argc - 1; i > 0; i--)
-	{
-		num = ft_atoi(argv[i]);
-		ft_stack_push(stack_a, ft_itov(num));
-	}
+	input = parse_input(argc, argv);
+
+    while (*input)
+    {
+        ft_stack_push(stack_a, ft_itov(ft_atoi(*input)));
+        input++;
+    }
+
+    ft_stack_print(stack_a);
+
 	ft_sort_stack(stack_a, stack_b);
 	ft_stack_clear(&stack_a);
 	ft_stack_clear(&stack_b);
